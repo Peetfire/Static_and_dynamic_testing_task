e12b14de0659dd9aa4fd8edf173ecf4dfc118010
petermordaunt <peetfire@hotmail.com>
2022-01-01 22:39:53 +0000

    Test file corrected and 1st test passed

--- part_2_code/tests/card_game_tests.py
+++ part_2_code/tests/card_game_tests.py
@@ -1,9 +1,11 @@
-from _typeshed import Self
+#from _typeshed import Self
 import unittest
+from unittest import result
+
 from src.card import Card
 from src.card_game import CardGame
 
-class TestCardGame:
+class TestCardGame(unittest.TestCase):
 
     def setUp(self):
         self.card1 = Card("hearts", 1)
@@ -11,12 +13,17 @@ class TestCardGame:
         self.card3 = Card("diamonds", 11)
         self.card4 = Card("spades", 9)
         self.hand = [self.card1, self.card2, self.card3, self.card4]
+        self.game = CardGame()
 
     def test_check_for_ace(self):
-        pass
+        expected = True
+        result = self.game.check_for_ace(self.card1)
+        self.assertEqual(expected, result)
 
+    @unittest.skip("not witten yet")     
     def test_highest_card(self):
         pass
-
+    
+    @unittest.skip("not witten yet")   
     def test_cards_total(self):
         pass
