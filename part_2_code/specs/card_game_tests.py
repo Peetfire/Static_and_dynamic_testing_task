e4d19dc11a355c296cd4375f6fb9999ded7deb7d
petermordaunt <peetfire@hotmail.com>
2022-01-01 22:17:54 +0000

    wrote setUp

--- part_2_code/specs/card_game_tests.py
+++ part_2_code/specs/card_game_tests.py
@@ -6,7 +6,11 @@ from src.card_game import CardGame
 class TestCardGame:
 
     def setUp(self):
-        pass
+        self.card1 = Card("hearts", 1)
+        self.card2 = Card("clubs", 5)
+        self.card3 = Card("diamonds", 11)
+        self.card4 = Card("spades", 9)
+        self.hand = [self.card1, self.card2, self.card3, self.card4]
 
     def test_check_for_ace(self):
         pass
