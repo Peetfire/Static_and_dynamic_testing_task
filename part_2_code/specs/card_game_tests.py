843728767b088bc8bd17ce1a7820f3bcaaf7b6cb
petermordaunt <peetfire@hotmail.com>
2022-01-01 22:12:19 +0000

    defined empty tests

--- part_2_code/specs/card_game_tests.py
+++ part_2_code/specs/card_game_tests.py
@@ -1,6 +1,18 @@
+from _typeshed import Self
 import unittest
 from src.card import Card
 from src.card_game import CardGame
 
 class TestCardGame:
-    pass
+
+    def setUp(self):
+        pass
+
+    def test_check_for_ace(self):
+        pass
+
+    def test_highest_card(self):
+        pass
+
+    def test_cards_total(self):
+        pass
